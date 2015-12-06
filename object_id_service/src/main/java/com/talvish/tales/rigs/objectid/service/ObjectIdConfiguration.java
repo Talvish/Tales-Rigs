b9e3b8289b0249613d4e0ebffe476992d5fe5add
Joseph Molnar <josephmolnar@hotmail.com>
2015-12-05 21:32:35 -0800

    updated config

---
+++ object_id_service/src/main/java/com/talvish/tales/rigs/objectid/service/ObjectIdConfiguration.java
@@ -0,0 +1,65 @@
+package com.talvish.tales.rigs.objectid.service;
+
+import java.util.List;
+
+import com.talvish.tales.serialization.OnDeserialized;
+import com.talvish.tales.system.configuration.annotated.Setting;
+
+/**
+ * The object id engine's configuration. 
+ * @author jmolnar
+ *
+ */
+public class ObjectIdConfiguration {
+//	//public static final String HOSTNAME = "id_source.hostname";
+//	public static final String SOURCE_ID_FORMAT = "id_source.%s.source_id"; // this takes the hostname
+//	
+//	//public static final String MAXIMUM_CACHE_AGE = "id_source.maximum_cache_age";
+//	//public static final int MAXIMUM_CACHE_AGE_DEFAULT = 86400;
+//	
+//	//public static final String DATA_DIRECTORY = "id_source.data_directory";
+//	//public static final String SUPPORTED_TYPES = "id_source.types";
+//	public static final String TYPE_DETAILS_FORMAT = SUPPORTED_TYPES + ".%s";
+//	public static final String TYPE_FILENAME_FORMAT = "%s.%s.%s.details"; // which is the source id, then type id, then type name
+//	
+//	public static final String TYPE_DETAILS_NAME = "name";
+//	public static final String TYPE_DETAILS_DESCRIPTION ="description";
+//	public static final String TYPE_DETAILS_ID = "id";
+//	
+//	
+//	
+//	
+//	@Setting( name="id_source.hostname", required=true )
+//	private String hostname = null; 
+//
+//	// it would be interesting if you could do something like this:
+//	//@Setting( name="id_source.{id_source.hostname}.source_id", required=true )
+//	private long sourceId = 0; 
+//
+//	@Setting( name="id_source.maximum_cache_age" )
+//	private int maximumCacheAge = 86400;
+//	
+//	@Setting( name="id_source.data_directory", required=true )
+//	private String dataDirectory;
+//	
+//	@Setting( name="id_source.types", required=true )
+//	private List<String> supportedTypes;
+//	
+//
+//	public String getHostname( ) {
+//		return hostname;
+//	}
+//	
+//	public int getMaximumCacheAge( ) {
+//		return maximumCacheAge;
+//	}
+//	
+//	public String getDataDirectory( ) {
+//		return dataDirectory;
+//	}
+//
+//	@OnDeserialized
+//	private void onDeserialized( ) {
+//		
+//	}
+}
